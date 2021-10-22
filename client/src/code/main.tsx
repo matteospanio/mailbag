594e12a8531edc7155ffd7b8a19f97cf1ca0aa9c
Matteo <877485@stud.unive.it>
2021-10-23 00:43:30 +0200

    workers setted up

--- client/src/code/main.tsx
+++ client/src/code/main.tsx
@@ -1,9 +1,10 @@
 import "normalize.css";
 import "../css/main.css";
 
+import React from "react";
 import ReactDOM from "react-dom";
-import BaseLayout from "./components/BaseLayout";
 
+import BaseLayout from "./components/BaseLayout";
 import * as IMAP from "./IMAP";
 import * as Contacts from "./Contacts";
 
